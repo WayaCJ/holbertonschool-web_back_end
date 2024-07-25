719476f45ca9f9fa3a9b8d036160cdc41849afe2
Carlos <sotocarlos1956@gmail.com>
2024-07-25 19:40:53 -0400

    saving in the cloud

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
-    const allEmployees = {
-      ...employeesList,
-    };
-    const getNumberOfDepartments = () => Object.keys(employeesList).length;
-    return {
-      allEmployees,
-      getNumberOfDepartments,
-    };
-  }
+  const allEmployees = {
+    ...employeesList,
+  };
+  const getNumberOfDepartments = () => Object.keys(employeesList).length;
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
