823239c2bbe93710af7594c18743386e66a6b88a
Carlos <sotocarlos1956@gmail.com>
2024-08-02 14:25:54 -0400

    saving

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,14 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-    resolve({
+      resolve({
         status: 200,
         body: 'Success',
-    });
+      });
     } else {
-    reject(new Error(
+      reject(new Error(
         'The fake API is not working currently',
-    ));
+      ));
     }
-});
+  });
 }
